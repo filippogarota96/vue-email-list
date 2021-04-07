4c10b4b1b7595dfb1911ec5b06f2f34ec750227f
Filippo Garota <filogarota101@gmail.com>
2021-04-07 16:11:06 +0200

    push di una mail dentro l'array

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 var app = new Vue ({
   el: '#app',
   data: {
-    mail: [],
+    email: []
   },
   mounted: function(){
     let self = this;
-  }
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+     .then(function (response) {
+       self.email.push(response.data.response);
+      
+   });
+ }
 }
 );
